1604c987e3bf10e6e022a46d6b1eb5cd7c94824f
Yevhenii Hladkevych <ggladko97@gmail.com>
2018-07-08 17:58:57 +0200

    Update Main.java
    reviewed file

--- src/main/java/com/database/Main.java
+++ src/main/java/com/database/Main.java
@@ -8,9 +8,11 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Main {
-    public static void main(String[] ar) throws SQLException {
+    //use ALT+SHIFT +L to auto-format  in intellij idea
+    public static void main(String[] args) throws SQLException {
         Connection connection;
 
+        //use private static final for constants. move just  bellow class declaration
            String url = "jdbc:mysql://localhost:3306/blink";
            String user = "root";
            String password = "password";
